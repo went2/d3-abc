2e2b3abdf4faa7b3f2bfa0c7f6fa971143174a5b
simon <simonfisher227@gmail.com>
2023-01-06 23:32:39 +0800

    feat: draw days

--- common-chart/heatmap/chart.js
+++ common-chart/heatmap/chart.js
@@ -124,6 +124,58 @@ async function drawHeatMap() {
         }, -10)`
     )
     .text((d) => monthFormat(d));
+
+  const dayOfWeekParse = d3.timeParse("%-e");
+  const dayOfWeekTickFormat = locale.format("%-a");
+  const labels = bounds
+    .selectAll(".label")
+    .data(new Array(7).fill(null).map((d, i) => i))
+    .enter()
+    .append("text")
+    .attr("class", "label")
+    .attr(
+      "transform",
+      (d) => `translate(-20, ${totalBarDimension * (d + 0.8)})`
+    )
+    .text((d) => dayOfWeekTickFormat(dayOfWeekParse(d)));
+
+  const drawDays = (metric) => {
+    d3.select("#metric").text(metric);
+    const colorAccessor = (d) => d[metric];
+    const colorRangeDomain = d3.extent(dataset, colorAccessor);
+    const colorRange = d3
+      .scaleLinear()
+      .domain(colorRangeDomain)
+      .range([0, 1])
+      .clamp(true);
+    const colorGradient = d3.interpolateHcl("#ecf0f1", "#5758BB");
+    const colorScale = (d) => colorGradient(colorRange(d) || 0);
+
+    d3.select("#legend-min").text(colorRangeDomain[0]);
+    d3.select("#legend-max").text(colorRangeDomain[1]);
+    d3.select("#legend-gradient").style(
+      "background",
+      `linear-gradient(to right, ${new Array(10)
+        .fill(null)
+        .map((d, i) => `${colorGradient(i / 9)} ${(i * 100) / 9}%`)
+        .join(", ")})`
+    );
+
+    const days = bounds.selectAll(".day").data(dataset, (d) => d.date);
+    const newDays = days.enter().append("rect");
+
+    const allDays = newDays
+      .merge(days)
+      .attr("class", "day")
+      .attr("x", (d) => totalBarDimension * xAccessor(d))
+      .attr("width", barDimension)
+      .attr("y", (d) => totalBarDimension * yAccessor(d))
+      .attr("height", barDimension)
+      .style("fill", (d) => colorScale(colorAccessor(d)));
+    const oldDots = days.exit().remove();
+  };
+
+  drawDays("moonPhase");
 }
 
 drawHeatMap();
