475b1f605c0de63e2b36d0bb99f2e54500a7071c
simon <simonfisher227@gmail.com>
2023-01-14 14:58:11 +0800

    create stacked bar chart

--- fundamentals/layout/src/App.vue
+++ fundamentals/layout/src/App.vue
@@ -3,7 +3,7 @@ import StackChart from "./components/StackChart.vue";
 import HistoChart from "./components/HistoChart.vue";
 import ViolinChart from "./components/ViolinChart.vue";
 import PieChart from "./components/PieChart.vue";
-import Streamgraph from "./components/streamgraph.vue";
+import Streamgraph from "./components/Streamgraph.vue";
 </script>
 
 <template>
--- fundamentals/layout/src/components/streamgraph.vue
+++ fundamentals/layout/src/components/streamgraph.vue
@@ -6,7 +6,9 @@ const dataColumn = ref([]);
 const dataset = ref([]);
 
 const xScale = d3.scaleLinear().domain([0, 10]).range([0, 500]);
-const yScale = d3.scaleLinear().domain([0, 100]).range([500, 0]);
+const yScale = d3.scaleLinear().domain([0, 60]).range([480, 0]);
+const heightScale = d3.scaleLinear().domain([0, 60]).range([0, 480]);
+
 const movies = [
   "titanic",
   "avatar",
@@ -25,8 +27,28 @@ const stackLayout = d3.stack().keys(movies);
 const stackArea = d3
   .area()
   .x((d, i) => xScale(i))
-  .y0((d) => yScale(d[0])) // 这里的d[0]是和 stackLayout 处理过的数据对应的
+  .y0((d) => yScale(d[0])) // y0 表示下界
   .y1((d) => yScale(d[1]));
+
+const drawAxis = () => {
+  const xAxis = d3.axisBottom().scale(xScale).ticks(10).tickSize(480);
+  const yAxis = d3.axisRight().scale(yScale).tickSize(500);
+
+  d3.select("svg")
+    .append("g")
+    .attr("id", "xAxisG")
+    .style("transform", "translate(0,480)")
+    .call(xAxis);
+
+  d3.select("svg")
+    .append("g")
+    .attr("id", "yAxisG")
+    // .style("transform", "translate(300,0)")
+    .call(yAxis);
+
+  d3.select("#xAxisG > path.domain").style("display", "none");
+  d3.select("#yAxisG > path.domain").style("display", "none");
+};
 
 onMounted(() => {
   d3.csv("/movies.csv").then((data) => {
@@ -38,7 +60,7 @@ onMounted(() => {
       });
       return item;
     });
-
+    drawAxis();
     console.log(stackLayout(dataset.value));
 
     // d3.select("svg")
@@ -49,23 +71,42 @@ onMounted(() => {
     //   .attr("d", stackArea)
     //   .style("fill", (d, i) => fillScale(i));
 
-    stackLayout.offset(d3.stackOffsetSilhouette).order(d3.stackOrderInsideOut);
-    stackArea.curve(d3.curveBasis);
-    yScale.domain([-50, 50]);
+    // stackLayout.offset(d3.stackOffsetSilhouette).order(d3.stackOrderInsideOut);
+    // stackArea.curve(d3.curveBasis);
+    // yScale.domain([-50, 50]);
 
+    // d3.select("svg")
+    //   .selectAll("path")
+    //   .data(stackLayout(dataset.value))
+    //   .enter()
+    //   .append("path")
+    //   .attr("d", stackArea)
+    //   .style("fill", (d, i) => fillScale(i));
+
+    // draw stacked bar chart
     d3.select("svg")
-      .selectAll("path")
+      .selectAll("g.bar")
       .data(stackLayout(dataset.value))
       .enter()
-      .append("path")
-      .attr("d", stackArea)
-      .style("fill", (d, i) => fillScale(i));
+      .append("g")
+      .each(function (d) {
+        d3.select(this)
+          .selectAll("rect")
+          .data(d)
+          .enter()
+          .append("rect")
+          .attr("x", (p, q) => xScale(q))
+          .attr("y", (p) => yScale(p[1]))
+          .attr("width", 40)
+          .attr("height", (p) => heightScale(p[1] - p[0]))
+          .style("fill", fillScale(d.key));
+      });
   });
 });
 </script>
 
 <template>
-  <svg id="svg" width="500px" height="500px"></svg>
+  <svg id="svg" width="550px" height="550px"></svg>
 </template>
 
 <style scoped></style>
