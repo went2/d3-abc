591edaec5e5b3535c1ed74c19ae1affea571a82d
simon <simonfisher227@gmail.com>
2023-01-25 23:25:18 +0800

    add graticule

--- complex data vis/geospatial vis/index.js
+++ complex data vis/geospatial vis/index.js
@@ -40,7 +40,7 @@ function createMap() {
         .selectAll("path")
         .data(countries.features)
         .join("path")
-        .attr("class", countries)
+        .attr("class", "countries")
         .attr("d", geoPath)
         .style("fill", (d) => countryColor(geoPath.area(d)))
         .style("stroke", (d) => d3.rgb(countryColor(geoPath.area(d))).darker());
@@ -52,6 +52,46 @@ function createMap() {
         .attr("r", 3)
         .attr("cx", (d) => projection([d.x, d.y])[0])
         .attr("cy", (d) => projection([d.x, d.y])[1]);
+
+      const graticule = d3.geoGraticule();
+      svg
+        .insert("path", "path.countries")
+        .datum(graticule)
+        .attr("class", "graticule line")
+        .attr("d", geoPath);
+      svg
+        .insert("path", "path.countries")
+        .datum(graticule.outline)
+        .attr("class", "graticlue outline")
+        .attr("d", geoPath);
+
+      // hover 显示国家的bounds box 和 中心点
+      svg
+        .selectAll("path.countries")
+        .on("mouseover", centerBounds)
+        .on("mouseout", clearCenterBounds);
+      function centerBounds(event, d) {
+        // console.log("mouseover", d);
+        const bounds = geoPath.bounds(d);
+        const center = geoPath.centroid(d);
+        svg
+          .append("rect")
+          .attr("class", "bbox")
+          .attr("x", bounds[0][0])
+          .attr("y", bounds[0][1])
+          .attr("width", bounds[1][0] - bounds[0][0])
+          .attr("height", bounds[1][1] - bounds[0][1]);
+        svg
+          .append("circle")
+          .attr("class", "centroid")
+          .attr("r", 5)
+          .attr("cx", center[0])
+          .attr("cy", center[1]);
+      }
+      function clearCenterBounds() {
+        d3.selectAll("circle.centroid").remove();
+        d3.selectAll("rect.bbox").remove();
+      }
     }
   });
 }
