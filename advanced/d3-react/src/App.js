a61e5fcf6a4c55ee2e89609c78b474a1e678955d
simon <simonfisher227@gmail.com>
2023-01-29 13:24:54 +0800

    add responsive

--- advanced/d3-react/src/App.js
+++ advanced/d3-react/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import BarChart from "./components/BarChart";
 import WorldMap from "./components/WorldMap";
+import StreamGraph from "./components/StreamGraph";
 
 import worldData from "./data/world";
 import { range, sum } from "d3-array";
 import { scaleThreshold } from "d3-scale";
 import { geoCentroid } from "d3-geo";
+import { useEffect, useState } from "react";
 
 const appData = worldData.features.filter((d) => geoCentroid(d)[0] < -20);
 
@@ -16,17 +18,49 @@ appData.forEach((d, i) => {
 });
 
 function App() {
+  const [size, setSize] = useState({ width: 1000, height: 500 });
+
+  function onResize() {
+    console.log("onresize");
+    setSize({
+      width: window.innerWidth,
+      height: window.innerHeight - 70,
+    });
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", onResize, false);
+    // setWidth(window.innerWidth);
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, [size]);
+
   const colorScale = scaleThreshold()
     .domain([5, 10, 20, 30, 50])
     .range(["#75739F", "#5EAFC6", "#41A368", "#93C464", "#FE9922"]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <h2>d3-in-action dashboard</h2>
+        <h2>Dashboard demo in d3-in-action chapter 9</h2>
       </header>
       <div>
-        <BarChart colorScale={colorScale} data={appData} size={[500, 400]} />
-        <WorldMap colorScale={colorScale} data={appData} size={[500, 400]} />
+        <StreamGraph
+          colorScale={colorScale}
+          data={appData}
+          size={[size.width, size.height / 2]}
+        />
+        <WorldMap
+          colorScale={colorScale}
+          data={appData}
+          size={[size.width * 0.48, size.height / 2]}
+        />
+        <BarChart
+          colorScale={colorScale}
+          data={appData}
+          size={[size.width * 0.48, size.height / 2]}
+        />
       </div>
     </div>
   );
---
+++ advanced/d3-react/src/components/StreamGraph.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import "../App.css";
+import {
+  stack,
+  area,
+  curveBasis,
+  stackOrderInsideOut,
+  stackOffsetSilhouette,
+} from "d3-shape";
+import { range } from "d3-array";
+import { scaleLinear } from "d3-scale";
+
+function StreamGraph({ data, size, colorScale }) {
+  const stackData = range(30).map(() => ({}));
+  for (let x = 0; x < 30; x++) {
+    data.forEach((country) => {
+      stackData[x][country.id] = country.data[x];
+    });
+  }
+  const xScale = scaleLinear().domain([0, 30]).range([0, size[0]]);
+  const yScale = scaleLinear().domain([0, 60]).range([size[1], 0]);
+
+  const stackLayout = stack()
+    .offset(stackOffsetSilhouette)
+    .order(stackOrderInsideOut)
+    .keys(Object.keys(stackData[0]));
+  const stackArea = area()
+    .x((d, i) => xScale(i))
+    .y0((d) => yScale(d[0]))
+    .y1((d) => yScale(d[1]))
+    .curve(curveBasis);
+
+  const stacks = stackLayout(stackData).map((d, i) => (
+    <path
+      key={"stack" + i}
+      d={stackArea(d)}
+      style={{
+        fill: colorScale(data[i].launchday),
+        stroke: "black",
+        strokeOpacity: 0.25,
+      }}
+    ></path>
+  ));
+  return (
+    <svg width={size[0]} height={size[1]}>
+      <g transform={`translate(0,${-size[1] / 2})`}>{stacks}</g>
+    </svg>
+  );
+}
+
+export default StreamGraph;
